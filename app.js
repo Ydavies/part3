61df982fdf14015d057c5defab089d05be171016
Ydavies <hiro.davies@gmail.com>
2025-05-26 12:50:22 +1000

    reading outpul folder

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 
 const fs = require('node:fs');
 
-//Declare data to be written
-const data = "This is a string to save to text file :)"
+// //Declare data to be written
+// const data = "This is a string to save to text file :)"
 
-// Writing an error to file
-console.log("Writing to file");
-// Function call
-fs.writeFile('output.txt', data, (err)=>{
-  // Check for errors
-  if (err){
-    console.error(err);
-  }else{
-    // Successful confirmation
-    console.log("File Written Successfully");
-  }});
+// // Writing an error to file
+// console.log("Writing to file");
+// // Function call
+// fs.writeFile('output.txt', data, (err)=>{
+//   // Check for errors
+//   if (err){
+//     console.error(err);
+//   }else{
+//     // Successful confirmation
+//     console.log("File Written Successfully");
+//   }});
 
 
 
